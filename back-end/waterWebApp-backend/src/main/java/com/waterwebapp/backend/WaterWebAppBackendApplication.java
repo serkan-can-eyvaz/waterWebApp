f26952953342d5634d013bdc5afafe505feaa639
serkan-can-eyvaz <serkancaneyvaz@gmail.com>
2025-09-30 23:45:59 +0300

    update

--- back-end/waterWebApp-backend/src/main/java/com/waterwebapp/backend/WaterWebAppBackendApplication.java
+++ back-end/waterWebApp-backend/src/main/java/com/waterwebapp/backend/WaterWebAppBackendApplication.java
@@ -2,10 +2,8 @@ package com.waterwebapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.waterwebapp.backend")
+@SpringBootApplication(scanBasePackages = {"com.waterwebapp.backend"})
 public class WaterWebAppBackendApplication {
 
     public static void main(String[] args) {
